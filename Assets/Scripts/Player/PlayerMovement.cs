914715d939332fd8bc46c7c9d62159e92688b4c6
jwashaba <166251049+jwashaba@users.noreply.github.com>
2025-04-11 22:03:56 -0700

    player animations and physics

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,30 +15,36 @@ public class PlayerMovement : MonoBehaviour {
 
     private bool canJump;
 
+    private Animator anim;
+
 
     // Start is called before the first frame update
     void Start() {
-       
+       anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
-         if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D)) {
             MoveSideways(Vector2.right);
             PlayerSR.flipX = true;
+            anim.SetBool("isWalking", true);
         }
         else if (Input.GetKey(KeyCode.A)) {
             MoveSideways(Vector2.left);
             PlayerSR.flipX = false;
+            anim.SetBool("isWalking", true);
         }
         else {
             MoveSideways(Vector2.zero);
+            anim.SetBool("isWalking", false);
         }
 
         if (Input.GetKeyDown(KeyCode.W) && canJump) {
             PlayerRB.linearVelocity = new Vector2(PlayerRB.linearVelocity.x, JumpStrength);
             canJump = false;
-        }
+            anim.SetBool("isJumping", true);
+        } 
     }
 
     private void MoveSideways(Vector2 direction) {
@@ -50,6 +56,7 @@ public class PlayerMovement : MonoBehaviour {
 
         if (collider.CompareTag("ground") || collider.CompareTag("interactable")) {
             canJump = true;
+            anim.SetBool("isJumping", false);
         }
     }
 }
