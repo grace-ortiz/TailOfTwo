015e3000ce8c948a90923365bad5b1db082f1714
grace-ortiz <gortiz@ucsd.edu>
2025-04-16 04:54:36 -0700

    adding enemies & respawn zones

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class Enemy : MonoBehaviour {
+    public float moveSpeed;
+    public float patrolDist;
+    public SpriteRenderer enemySR;
+    public Rigidbody2D enemyRB;
+    public float idleTime;
+
+    private Vector3 startingPosition;
+    private int direction = 1;
+
+    private float waitTimer = 0f;
+    private bool isWaiting = false;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+        if (isWaiting)
+        {
+            waitTimer += Time.deltaTime;
+            if (waitTimer >= idleTime)
+            {
+                float clampedX = startingPosition.x + direction * patrolDist;
+                transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+
+                isWaiting = false;
+                waitTimer = 0f;
+                direction *= -1;
+                enemySR.flipX = !enemySR.flipX;
+            }
+            return;
+        }
+
+        transform.Translate(Vector3.right * direction * moveSpeed * Time.deltaTime);
+
+        if (Mathf.Abs(transform.position.x - startingPosition.x) >= patrolDist) {
+            isWaiting = true;
+        }
+    }
+
+}
