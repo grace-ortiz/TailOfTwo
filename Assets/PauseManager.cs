cb64f6e9295b63cf839ffb891fb6784c08ffa7eb
jwashaba <166251049+jwashaba@users.noreply.github.com>
2025-04-16 00:09:21 -0700

    pause menu sprites implemented

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject PauseMenuUI;
+
+    public PlayerMovement playerMovement;
+    public Slider SliderMusic;
+    public Slider SliderSFX;
+    private bool isVisible;
+
+    void Start() 
+    {
+        isVisible = false;
+        PauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) TogglePause();
+    }
+
+    // public void ExitGame()
+    // {
+    //     GameManager.Instance.ExitGame();
+    // }
+
+    public void TogglePause()
+    {
+        if(!isVisible) // if not visible, pause game
+        {
+            PauseMenuUI.SetActive(true);
+            isVisible = true;
+            Time.timeScale = 0f;
+        }
+        else // if visible, resume game
+        {
+            PauseMenuUI.SetActive(false);
+            isVisible = false;
+            Time.timeScale = 1f;
+        }
+    }
+
+    // public void SetMusicVolume() { AudioManager.Instance.ChangeMusicVolume(SliderMusic.value); }
+    // public void SetSFXVolume() { AudioManager.Instance.ChangeSFXVolume(SliderSFX.value); }
+}
