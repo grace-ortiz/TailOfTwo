d58f83da0e6c8cbc8ffd7a13ce06688c59cb7f25
grace-ortiz <gortiz@ucsd.edu>
2025-04-18 20:26:55 -0700

    pools only interacted with once

--- Assets/Scripts/Interactables/Pool.cs
+++ Assets/Scripts/Interactables/Pool.cs
@@ -4,12 +4,21 @@ using System.Collections.Generic;
 public class Pool : Interactable {
 
     [SerializeField] private Transform mainRespawnPoint;
+
+    private bool isInteracted = false;
+
+    public void Start() {
+        isInteracted = false;
+    }
     public void Interact()
     {
+        if (isInteracted) return;
+
         PlayerBehavior player = FindFirstObjectByType<PlayerBehavior>();
         if (player != null)
         {
             player.SetMaxCharges(player.maxCharges + 1);
+            isInteracted = true;
 
             if (mainRespawnPoint != null)
             {
